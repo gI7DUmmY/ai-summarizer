5092c82d072bfa24d15b303406f2bb0236dcacb5
Cedric Rollo <cedric.rollo@gmail.com>
2023-09-03 14:40:28 +0200

    github url

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,9 @@ const Hero = () => {
 
         <button
           type='button'
-          onClick={() => window.open('#')}
+          onClick={() =>
+            window.open('https://github.com/gI7DUmmY/ai-summarizer')
+          }
           className='black_btn'
         >
           GitHub
